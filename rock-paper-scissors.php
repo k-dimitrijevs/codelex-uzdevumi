6172ec692d0a3adb02b0fba9b6ad3a9c804128df
k-dimitrijevs <kristians.dimitrijevs@gmail.com>
2021-08-25 10:23:24 +0300

    rock-paper-scissors

--- rock-paper-scissors.php
+++ rock-paper-scissors.php
@@ -4,23 +4,36 @@
  * Rock-paper-scissors game.
  */
 
-$rps = ["rock", "paper", "scissors"];
-shuffle($rps);
-$computerInput = $rps[0];
-echo "COMPUTER: " . $computerInput . PHP_EOL;
+$playAgain = "y";
+
+while ($playAgain = "y"){
+    $rps = ["rock", "paper", "scissors"];
+    shuffle($rps);
+    $computerInput = $rps[0];
+    echo "COMPUTER: " . $computerInput . PHP_EOL;
 
 
-$userInput = readline("Rock, Paper, Scissors?: ");
-$userInput = strtolower($userInput);
+    $userInput = readline("Rock, Paper, Scissors?: ");
+    $userInput = strtolower($userInput);
 
-if ($userInput === $computerInput) {
-    echo "It's a tie!" . PHP_EOL;
-} elseif ($userInput === "rock" && $computerInput === "scissors") {
-    echo "You won! Computer picked {$computerInput}" . PHP_EOL;
-} elseif ($userInput === "paper" && $computerInput === "rock") {
-    echo "You won! Computer picked {$computerInput}" . PHP_EOL;
-} elseif ($userInput === "scissors" && $computerInput === "paper") {
-    echo "You won! Computer picked {$computerInput}" . PHP_EOL;
-} else {
-    echo "You lost! Computer picked {$computerInput}" . PHP_EOL;
+
+    if ($userInput === $computerInput) {
+        echo "It's a tie!" . PHP_EOL;
+    } elseif ($userInput === "rock" && $computerInput === "scissors") {
+        echo "You won! Computer picked {$computerInput}" . PHP_EOL;
+    } elseif ($userInput === "paper" && $computerInput === "rock") {
+        echo "You won! Computer picked {$computerInput}" . PHP_EOL;
+    } elseif ($userInput === "scissors" && $computerInput === "paper") {
+        echo "You won! Computer picked {$computerInput}" . PHP_EOL;
+    } else {
+        echo "You lost! Computer picked {$computerInput}" . PHP_EOL;
+    }
+
+    $playAgain = readline("Play again? (y / n): ");
+    if ($playAgain !== "y") {
+        exit;
+    }
+
 }
+
+
