2c0fc24bca0eb8bf7d340d3ed1292f262688221c
k-dimitrijevs <kristians.dimitrijevs@gmail.com>
2021-09-04 15:26:45 +0300

    Horse racing simulator

--- horse-racing/horse-racing.php
+++ horse-racing/horse-racing.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  * Ievade - cik spēlētāju
  * Katram spēlētājam piešķirtam unikālu simolu vai skaitli, vai burtu.
@@ -17,16 +16,20 @@
  */
 
 while (true) {
-    $numOfHorses = (int)readline("Provide number of players that will race (max - 26): ");
-    if ($numOfHorses < 1 || $numOfHorses > 26) {
+    $numOfHorses = (int)readline("Provide number of players that will race (max - 10): ");
+    if ($numOfHorses < 1 || $numOfHorses > 10) {
         echo "Invalid number of players! Try Again!" . PHP_EOL;
     } else {
         break;
     }
 }
 
-$horses = range("A", "Z");
-$trackLen = 10;
+$player = new stdClass();
+$player->balance = 500;
+$player->bets = [];
+
+$horses = range("A", "J");
+$trackLen = 15;
 $fieldTrack = [];
 
 
@@ -38,23 +41,28 @@ for ($i = 1; $i <= $numOfHorses; $i++) {
     $fieldTrack[] = $runnerTrack;
 }
 
-$coef = [];
+$horseAndBet = [];
 
 for ($i = 0; $i < $numOfHorses; $i++) {
     $fieldTrack[$i][0] = $horses[$i];
-    array_push($coef, [$horses[$i] => rand(1, 7)]);
+    //array_push($horseAndBet, [$horses[$i] => rand(1, 7)]);
+    $horseAndBet[] = [$horses[$i] => rand(1, 8)];
 
 }
-var_dump($coef);
 
-foreach ($fieldTrack as $runnerLine) {
-    foreach ($runnerLine as $length) {
-        echo $length;
+echo "------------ HORSES AND COEFFICIENTS ------------" . PHP_EOL;
+foreach ($horseAndBet as $key => $hab)
+{
+    foreach($hab as $keyNext => $value)
+    {
+        echo "NAME: {$keyNext}  ||  COEFFICIENT: {$value}" . PHP_EOL;
     }
-    echo PHP_EOL;
 }
+
+echo "-------------------------------------------------" . PHP_EOL;
 
 while (true) {
+    echo "YOUR BALANCE: {$player->balance}" . PHP_EOL;
     $betHorse = readline("Choose a horse to bet on: ");
     for ($i = 0; $i < $numOfHorses; $i++) {
         $fieldTrack[$i][0] = $horses[$i];
@@ -63,18 +71,34 @@ while (true) {
             continue;
         } else {
             $msg = "Horse selected!" . PHP_EOL;
-            $betAmount = (int) readline("Enter bet amount!: ");
+            $betAmount = (int) readline("Enter bet amount: ");
+            if ($betAmount > $player->balance) {
+                $msg = "Not enough money!" . PHP_EOL;
+            } else {
+                $msg = "Bet placed on {$betHorse}!" . PHP_EOL;
+                $player->balance -= $betAmount;
+                $player->bets[$betHorse] = $betAmount;
+            }
         }
         break;
     }
     echo $msg . PHP_EOL;
 
-    echo $betHorse . PHP_EOL;
-    $betAgain = readline("Bet more? (y/n): ");
+    $betAgain = readline("Bet again? (y/n): ");
     if ($betAgain !== "y") {
         break;
     }
 }
+
+echo "------------ LET'S GO ------------" . PHP_EOL;
+
+foreach ($fieldTrack as $runnerLine) {
+    foreach ($runnerLine as $length) {
+        echo $length;
+    }
+    echo PHP_EOL;
+}
+echo PHP_EOL;
 
 $move = 0;
 $winners = [];
@@ -110,6 +134,7 @@ while (count($winners) < $numOfHorses) {
         }
         echo PHP_EOL;
     }
+    echo PHP_EOL;
 }
 
 echo "Results: " . PHP_EOL;
@@ -125,13 +150,17 @@ for ($i = 1; $i <= $numOfHorses; $i++) {
     }
 }
 
-foreach ($coef as $c) {
-
-    if ($winners[0] === $betHorse) {
-        $winnings = $betAmount * $c[$betHorse];
-        echo "You won! Your winnings are {$winnings}$" . PHP_EOL;
-    } else {
-        echo "You lost!" . PHP_EOL;
+$winnings = 0;
+echo "Horse {$winners[0]} won the race!" . PHP_EOL;
+foreach ($player->bets as $key => $bet) {
+    if ($winners[0] === $key) {
+        foreach ($horseAndBet as $item) {
+            foreach ($item as $test => $i) {
+                if ($test === $key) {
+                    $winnings = $player->bets[$key] * $i;
+                    echo "You won! Your winnings are {$winnings}$" . PHP_EOL;
+                }
+            }
+        }
     }
-    exit;
 }
